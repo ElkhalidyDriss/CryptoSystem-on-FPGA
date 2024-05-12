bcc5e156c1bd54a08c02cd609b39e97aea8ee23d
Idriss <idrisskhaldi75@gmail.com>
2024-05-12 19:34:27 +0100

    Added the entity of subByte

--- src/shiftRows.vhd
+++ src/shiftRows.vhd
@@ -1,32 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-
-entity shiftRows is 
-port (
-       data_in : in std_logic_vector(127 downto 0);
-       data_out : out std_logic_vector(127 downto 0)
-
-);
+entity shiftRows is
+    port (
+        i_data : in std_logic_vector(127 downto 0); --input data
+        o_data : out std_logic_vector(127 downto 0) --output data
+    );
 end entity;
 
-architecture shiftRows_arch of shiftRows is 
-
+architecture shiftRowsArch of shiftRows is
 begin
-     data_out(8*1 - 1 downto 8*0)   <= data_in(8*1 - 1 downto 8*0);
-     data_out(8*2 - 1 downto 8*1)   <= data_in(8*6 - 1 downto 8*5);
-     data_out(8*3 - 1 downto 8*2)   <= data_in(8*11 - 1 downto 8*10);
-     data_out(8*4 - 1 downto 8*3)   <= data_in(8*16 - 1 downto 8*15);
-     data_out(8*5 - 1 downto 8*4)   <= data_in(8*5 - 1 downto 8*4);
-     data_out(8*6 - 1 downto 8*5)   <= data_in(8*10 - 1 downto 8*9);
-     data_out(8*7 - 1 downto 8*6)   <= data_in(8*15 - 1 downto 8*14);
-     data_out(8*8 - 1 downto 8*7)   <= data_in(8*4 - 1 downto 8*3);
-     data_out(8*9 - 1 downto 8*8)   <= data_in(8*9 - 1 downto 8*8);
-     data_out(8*10 - 1 downto 8*9)  <= data_in(8*14 - 1 downto 8*13);
-     data_out(8*11 - 1 downto 8*10) <= data_in(8*3 - 1 downto 8*2);
-     data_out(8*12 - 1 downto 8*11) <= data_in(8*8 - 1 downto 8*7);
-     data_out(8*13 - 1 downto 8*12) <= data_in(8*13 - 1 downto 8*12);
-     data_out(8*14 - 1 downto 8*13) <= data_in(8*2 - 1 downto 8*1);
-     data_out(8*15 - 1 downto 8*14) <= data_in(8*7 - 1 downto 8*6);
-     data_out(8*16 - 1 downto 8*15) <= data_in(8*12 - 1 downto 8*11);     
-end architecture ;
+    o_data(8*1 - 1 downto 8*0)    <= i_data(8*1 - 1 downto 8*0);
+    o_data(8*2 - 1 downto 8*1)    <= i_data(8*6 - 1 downto 8*5);
+    o_data(8*3 - 1 downto 8*2)    <= i_data(8*11 - 1 downto 8*10);
+    o_data(8*4 - 1 downto 8*3)    <= i_data(8*16 - 1 downto 8*15);
+    o_data(8*5 - 1 downto 8*4)    <= i_data(8*5 - 1 downto 8*4);
+    o_data(8*6 - 1 downto 8*5)    <= i_data(8*10 - 1 downto 8*9);
+    o_data(8*7 - 1 downto 8*6)    <= i_data(8*15 - 1 downto 8*14);
+    o_data(8*8 - 1 downto 8*7)    <= i_data(8*4 - 1 downto 8*3);
+    o_data(8*9 - 1 downto 8*8)    <= i_data(8*9 - 1 downto 8*8);
+    o_data(8*10 - 1 downto 8*9)   <= i_data(8*14 - 1 downto 8*13);
+    o_data(8*11 - 1 downto 8*10)  <= i_data(8*3 - 1 downto 8*2);
+    o_data(8*12 - 1 downto 8*11)  <= i_data(8*8 - 1 downto 8*7);
+    o_data(8*13 - 1 downto 8*12)  <= i_data(8*13 - 1 downto 8*12);
+    o_data(8*14 - 1 downto 8*13)  <= i_data(8*2 - 1 downto 8*1);
+    o_data(8*15 - 1 downto 8*14)  <= i_data(8*7 - 1 downto 8*6);
+    o_data(8*16 - 1 downto 8*15)  <= i_data(8*12 - 1 downto 8*11);     
+end architecture;
